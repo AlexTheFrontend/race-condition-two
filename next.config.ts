22b741e760b4e62c157732b5c2e730433980fee0
Alexander <45053518+AlexTheFrontend@users.noreply.github.com>
2025-10-21 13:47:42 +1300

    Add redirect from root to rc2.html

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/rc2.html',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
